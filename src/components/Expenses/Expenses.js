1dad3b1918ee981587e147aee46a1fc2e64a05bc
Pamela Quiros <pamelaquirosu@hotmail.com>
2022-09-14 15:21:19 -0600

    Arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './Expenses.css'
 
-function items (props) {
+const items = (props)  => {
 return (
     <Card className="expenses">
     <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
